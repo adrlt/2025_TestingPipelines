322a2c8d5f47401ad5130f2416459fe9358dc1a1
adrlt <reyhnlupa@gmail.com>
2025-02-20 15:00:21 +0100

    Slight improvements to scripts

--- 98_Scripts/98_Get-SRA-Sequences.sh
+++ 98_Scripts/98_Get-SRA-Sequences.sh
@@ -1,22 +1,27 @@
 #!/bin/bash -l
-# This script allows the user to download sequences from SRA
+#  This script allows the user to download sequences from SRA
 
-#SBATCH --account 2025_TestingPipelines		# Charge resources used by this job to specified account
+##  Declaring Slurm Configuration Options
+#SBATCH --job-name=DLSRASEQ
+#SBATCH --time=0-06:00:00
+#SBATCH --partition=cpu
+#SBATCH --cpus-per-task=1	# Default memory allocated is 8GB/CPU
+
+#SBATCH --output=/work/FAC/FBM/DEE/mrobinso/default/pminguet/Projects/2025_TestingPipelines/97_Logs/00_%x-%j.out
+#SBATCH --error=/work/FAC/FBM/DEE/mrobinso/default/pminguet/Projects/2025_TestingPipelines/97_Logs/00_%x-%j.err
+#  %x is a variable that fills in your job name
+#  %j is a variable that fills in your job ID number
+
 #SBATCH --mail-type NONE			# Notifications won't be sent by mail
-
 #SBATCH --chdir /scratch/pminguet		# Set the WD of the batch script to directory before is is executed
-#SBATCH --job-name DL-SRA-Seqs			# Give your job a name
-#SBATCH --output DL-SRA-Seqs.out		# Define the name of the output file
 
-#SBATCH --partition cpu
-
-#SBATCH --cpus-per-task 1			# Choose how many CPUs per task
-#SBATCH --mem 8G				# Choose the memory to use
-#SBATCH --time 3:30:00				# Choose a time limit for the job allocation (will remain in PENDNING state if time limit exceeded)
-#SBATCH --export NONE				# Only SLURM and SPANK option variables from the user environment will be defined
-
+##  Loading Software/Libraries
+#  Loading SRAToolKit v3.0.0
 module load sratoolkit/3.0.0
 
+##  Running Code
 for AccNum in /work/FAC/FBM/DEE/mrobinso/default/pminguet/Projects/2025_TestingPipelines/95_RawData/AccessionLists/BioProjectVenomAccList.txt
 
 fasterq-dump 
+
+#  SCRIPT NOT FINISHED
